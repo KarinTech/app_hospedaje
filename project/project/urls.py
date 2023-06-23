2c9d11dddcdcd8ecfa1cbddf1c7d4dfb23a89c79
Karin Centenaro <centenarokarin7@gmail.com>
2023-06-23 15:51:16 -0300

    agrego paths en urls.py

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import LandingPage, BarilochePage, CarlospazPage, SaltaPage
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPage.as.view (), name="landing-page"),
+    path('', BarilochePage.as.view(), name="bariloche-page"),
+    path('', CarlospazPage.as.view(), name="carlospaz-page")
 ]
